aa45630a2d4fe34c94e8e9179b2f5247d8d2fc8f
kimg9 <kim.gemo@gmail.com>
2025-11-03 10:11:58 +0100

    update urls namespaces to apps changes

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from . import views
+from .apps import LettingsConfig
+
+app_name = LettingsConfig.name
 
 urlpatterns = [
-    path('lettings/', views.index, name='lettings_index'),
+    path('lettings/', views.index, name='index'),
     path('lettings/<int:letting_id>/', views.letting, name='letting'),
 ]
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .apps import ProfilesConfig
 
+app_name = ProfilesConfig.name
 urlpatterns = [
-    path('profiles/', views.index, name='profiles_index'),
+    path('profiles/', views.index, name='index'),
     path('profiles/<str:username>/', views.profile, name='profile'),
 ]
