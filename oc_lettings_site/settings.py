4922093e46ee160e27405baf887218dc465af68a
kimg9 <kim.gemo@gmail.com>
2026-01-12 17:06:15 +0100

    change storage variables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -128,6 +128,7 @@ if not DEBUG:
 
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+print(f"Le nom du buckest est: {AWS_STORAGE_BUCKET_NAME}")
 
 if AWS_STORAGE_BUCKET_NAME:
     INSTALLED_APPS += ['storages']
@@ -137,7 +138,15 @@ if AWS_STORAGE_BUCKET_NAME:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Stockage des fichiers statiques
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        },
+    }
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"  # noqa: E231
 
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
